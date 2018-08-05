342b82d7b043cbc8efb87d4bf3941b6a3ebed8c1
unknown <vanessa.cassandra.s@gmail.com>
2018-08-05 20:58:49 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT ||5000;
+
+app.get('/api/hello', (req, res)=>{
+    res.send({express: 'Hello from express'});
+});
+
+if(process.env.NODE_ENV === 'production'){
+    //serve any static file
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    //Handle react routing, return all requests to React app
+    app.get('*', function(req, res){
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(port);
