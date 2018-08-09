bc5818242f5e024f35601da96a54326d263295aa
unknown <vanessa.cassandra.s@gmail.com>
2018-08-09 12:50:01 +0800

    Styled the about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import './About.css';
 
 class About extends Component{
     render(){
         return(
             <div className="About">
-                <h2>About</h2>
+                <div className="Profile">
+                    <h2>Hi, my name is Vanessa.<br/>I'm an Automation Application Engineer.</h2>
+                    That means, I help people build machines.
+                </div>
             </div>
         );
     }
