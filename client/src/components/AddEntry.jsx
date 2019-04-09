dfb0783882f702804a974630df42a5495ffdfd40
Vanessa <vanessa@pebbleroad.com>
2019-04-09 14:20:14 +0800

    Add some improvements

--- client/src/components/AddEntry.jsx
+++ client/src/components/AddEntry.jsx
@@ -1,31 +1,47 @@
-import React, {Component} from 'react';
-import './AddEntry.css';
+import React, { Component } from "react";
+import "./AddEntry.css";
 
-class AddEntry extends Component{
-    constructor(props){
-        super(props);
-        this.state = {entry: ''};
-        this.add = this.add.bind(this);
-        this.updateState = this.updateState.bind(this);
-    }
+class AddEntry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { entry: "" };
+    this.add = this.add.bind(this);
+    this.updateState = this.updateState.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+  }
 
-    add(){
-        this.props.addTodo(this.state.entry);
-        this.setState({entry: ''});
-    }
+  add() {
+    this.props.addTodo(this.state.entry);
+    this.setState({ entry: "" });
+  }
 
-    updateState(event){
-        this.setState({entry: event.target.value});
-    }
+  updateState(event) {
+    this.setState({ entry: event.target.value });
+  }
 
-    render(){
-        return(
-            <div className="AddEntry">
-                <input type="text" className="entry-textbox" onChange={this.updateState} value={this.state.entry} placeholder="Write a task here"/>
-                <button type="submit" className="add-btn" onClick={this.add}>Add</button>
-            </div>
-        );
+  checkEnter(event) {
+    if (event.key === "Enter") {
+      this.add();
     }
+  }
+
+  render() {
+    return (
+      <div className="AddEntry">
+        <input
+          type="text"
+          className="entry-textbox"
+          onChange={this.updateState}
+          onKeyPress={this.checkEnter}
+          value={this.state.entry}
+          placeholder="Write a task here"
+        />
+        <button type="submit" className="add-btn" onClick={this.add}>
+          Add
+        </button>
+      </div>
+    );
+  }
 }
 
 export default AddEntry;
--- client/src/components/TodoAppList.jsx
+++ client/src/components/TodoAppList.jsx
@@ -1,74 +1,86 @@
-import React, {Component} from 'react';
-import './TodoAppList.css';
-import Todo from './Todo.jsx';
-import AddEntry from './AddEntry.jsx';
+import React, { Component } from "react";
+import "./TodoAppList.css";
+import Todo from "./Todo.jsx";
+import AddEntry from "./AddEntry.jsx";
 
-class TodoAppList extends Component{
-    constructor(props){
-        super(props);
-        this.state={todo: [{entry:'Use mongo', status: 0}, {entry: 'Make it fancy', status: 0}, {entry: 'Deploy to Heroku', status: 1}]};
-        this.addTodo = this.addTodo.bind(this);
-        this.removeTodo = this.removeTodo.bind(this);
-        this.check = this.check.bind(this);
-        this.uncheck = this.uncheck.bind(this);
+class TodoAppList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: [
+        { entry: "Use mongo", status: 0 },
+        { entry: "Make it fancy", status: 0 },
+        { entry: "Deploy to Heroku", status: 1 }
+      ]
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+    this.check = this.check.bind(this);
+    this.uncheck = this.uncheck.bind(this);
+  }
+
+  addTodo(newEntry) {
+    if (newEntry !== "") {
+      let newTodo = this.state.todo;
+      newTodo.unshift({ entry: newEntry, status: 0 });
+      this.setState({
+        todo: newTodo
+      });
+    } else {
+      this.placeholder = "Please enter a task";
     }
+  }
 
-    addTodo(newEntry){
-        if(newEntry !== ''){
-            this.setState({todo:[...this.state.todo, {entry: newEntry, status: 0}]});
-        }else{
-            this.placeholder = "Please enter a task";
-        }        
-    }
+  removeTodo(entry) {
+    const filteredEntries = this.state.todo.filter(object => {
+      return object.entry !== entry;
+    });
 
-    removeTodo(entry){
-        const filteredEntries = this.state.todo.filter(object => {
-            return object.entry !== entry;
-        });
+    this.setState({ todo: filteredEntries });
+  }
 
-        this.setState({todo: filteredEntries});
-    }
+  check(entry) {
+    //Delete
+    let filteredEntries = this.state.todo.filter(object => {
+      return object.entry !== entry;
+    });
+    //Add again with new status
+    filteredEntries.push({ entry: entry, status: 1 });
 
-    renderList(){
-        return(
-            this.state.todo.map((object, index)=>(
-                <Todo key={index} entry={object.entry} status={object.status} removeTodo={this.removeTodo} check={this.check} uncheck={this.uncheck}/>
-            )
-        ));
-    }
+    this.setState({ todo: filteredEntries });
+  }
 
-    check(entry){
-        //Delete 
-        let filteredEntries = this.state.todo.filter(object=>{
-            return object.entry !== entry;
-        })
-        //Add again with new status
-        filteredEntries.push({entry: entry, status: 1});
+  uncheck(entry) {
+    //Delete
+    let filteredEntries = this.state.todo.filter(object => {
+      return object.entry !== entry;
+    });
+    //Add again with new status
+    filteredEntries.unshift({ entry: entry, status: 0 });
 
-        this.setState({todo: filteredEntries});
-    }
+    this.setState({ todo: filteredEntries });
+  }
 
-    uncheck(entry){
-        //Delete
-        let filteredEntries = this.state.todo.filter(object=>{
-            return object.entry !== entry;
-        })
-        //Add again with new status
-        filteredEntries.unshift({entry: entry, status: 0});
-
-        this.setState({todo: filteredEntries});
-    }
-
-    render(){
-        return(
-            <div className="TodoAppList">
-                <AddEntry addTodo={this.addTodo} placeholder={this.placeholder}/>
-                <div className="Todo-title"><h2>To-do:</h2></div>
-                {this.renderList()}
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="TodoAppList">
+        <AddEntry addTodo={this.addTodo} placeholder={this.placeholder} />
+        <div className="Todo-title">
+          <h2>To-do:</h2>
+        </div>
+        {this.state.todo.map((object, index) => (
+          <Todo
+            key={index}
+            entry={object.entry}
+            status={object.status}
+            removeTodo={this.removeTodo}
+            check={this.check}
+            uncheck={this.uncheck}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default TodoAppList;
